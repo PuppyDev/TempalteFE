29f5f6ee79ca7dc65a5d6d2c5e05c77a11c32c07
quocbao <johndoe@example.comgit>
2021-08-07 09:39:37 +0700

    uglify JS

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,22 @@ var browserSync = require('browser-sync').create();
 var rename = require("gulp-rename");
 var cleanCSS = require('gulp-clean-css');
 var pug = require('gulp-pug');
+// var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+
+gulp.task('mini-JS', function () {
+
+    return gulp.src('js/*.js')
+        .pipe(uglify())
+        .pipe(
+            rename({
+                suffix: ".min",
+            })
+        )
+        .pipe(gulp.dest('js/dest'))
+
+})
+
 // Sass compile into Css
 gulp.task('scss', function () {
     return gulp.src('scss/**/*.scss')
@@ -37,14 +53,14 @@ gulp.task('watch', function () {
     });
 
     // run first time you call this task 
-    gulp.parallel("scss", "pugToHtml")()
+    gulp.parallel("scss", "pugToHtml", "mini-JS")()
     // watch when change 
     gulp.watch('./scss/**/*.scss', gulp.series('scss'));
-    // gulp.watch("./js/**/*.js", gulp.series('mini-JS'));
+    gulp.watch("./js/*.js", gulp.series('mini-JS'));
     gulp.watch("./pug/**/*.pug", gulp.series('pugToHtml'));
     gulp.watch("*.html").on("change", browserSync.reload);
     gulp.watch("html/**/*.html").on("change", browserSync.reload);
     gulp.watch("scss/**/*.scss").on("change", browserSync.reload);
-    gulp.watch("js/**/*.js").on("change", browserSync.reload);
+    gulp.watch("js/*.js").on("change", browserSync.reload);
 
 })
